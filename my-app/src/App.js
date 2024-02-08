fab4da071e175d14df10401a8d7be83fab12972f
NovorusovaKsuniya <Ks21072004@yandex.ru>
2024-02-08 21:13:49 +0300

    №1-№11

---
+++ my-app/src/App.js
@@ -0,0 +1,68 @@
+import React from 'react';
+
+function App() {
+  /*return	<ul>
+    <li>1</li>
+    <li>2</li>
+    <li>3</li>
+    <li>4</li>
+    <li>5</li>
+    <li>6</li>
+    <li>7</li>
+    <li>8</li>
+    <li>9</li>
+    <li>10</li>
+		</ul>;
+    (8)*/
+  /*return(
+		<ul>
+			<li>text1</li>
+			<li>text2</li>
+			<li>text3</li>
+		</ul>
+  )
+  (9)*/
+  /*return<ul>
+   <ul>
+		<li>text1</li>
+		<li>text2</li>
+		<li>text3</li>
+	</ul>
+	<ul>
+		<li>text1</li>
+		<li>text2</li>
+		<li>text3</li>
+	</ul>;
+  </ul>
+  return<>
+   <ul>
+		<li>text1</li>
+		<li>text2</li>
+		<li>text3</li>
+	</ul>
+	<ul>
+		<li>text1</li>
+		<li>text2</li>
+		<li>text3</li>
+	</ul>;
+  </>
+
+  
+  
+  return <>
+  <ul>
+		<li>text1</li>
+		<li>text2</li>
+		<li>text3</li>
+	</ul>
+	<ul>
+		<li>text1</li>
+		<li>text2</li>
+		<li>text3</li>
+	</ul>;
+  </> 
+  (10)*/
+  return <input/>;// #11
+}
+
+export default App;
